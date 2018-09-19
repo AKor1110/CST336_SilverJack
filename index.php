2b35d161a783c3566827b715074deb11a80271cb
Jeremy S <jeshaw@csumb.edu>
2018-09-19 22:32:32 +0000

    Updating index...

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+
+include "core/Game.php";
+
+
+
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Silver Jack</title>
+        <meta charset="utf-8"/>
+        <link href="css/styles.css" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+        <header></header>
+        <div class="cardDisplay">
+            
+        </div>
+        
+        
+        <footer>
+            <hr>
+            CST 336: Internet Programming &copy; 2018 <br />
+            <strong>Disclaimer:</strong> The information in this website is fake.
+            <br />
+            It is for educational purposes only.
+            <br />
+        </footer>
+    </body>
+</html>
