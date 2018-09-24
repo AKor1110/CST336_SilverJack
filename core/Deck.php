ae9f55a68eb0fc20a02f81d086980fc8b123ba47
Andy Kor <akor@csumb.edu>
2018-09-24 20:12:14 +0000

    Added Deck class

---
+++ core/Deck.php
@@ -0,0 +1,29 @@
+<?php 
+
+class Deck {
+    // $deck will be a variable that holds a Stack object
+    private $deck = null;
+    
+    public function __construct() {
+    
+    }
+    
+    public function getCard() {
+        // Arguments: None
+        // Abstract: returns a Card object from the top of the Stack
+    }
+    
+    
+    private function makeDeck() {
+        // Arguments: None
+        // Abstract: returns an array of randomized Card objects
+    }
+    
+    private function addDecktoStack() {
+        //
+    }
+    
+}
+
+
+?>
