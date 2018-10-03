688bcf05692c4b0a17ff31271c797ecf4f3e5fd4
Elizabeth Limon <elimon@csumb.edu>
2018-10-03 17:26:39 +0000

    new

--- index.php
+++ index.php
@@ -5,27 +5,24 @@
         <meta charset="utf-8"/>
         <link href="css/styles.css" rel="stylesheet" type="text/css">
     </head>
+    <header>
+        <h1>Silver Jack</h1>
+    </header>
     <body>
-        <header></header>
+       
         
         <?php
             include "core/Game.php";
-            
-            
-            
-            
-            
-            
             
         ?>
         
         
         <footer>
             <hr>
-            CST 336: Internet Programming &copy; 2018 <br />
-            <strong>Disclaimer:</strong> The information in this website is fake.
+            CST 336: Group Project<br />
+            <strong>Members:</strong> <small>Elizabeth Limon, Andy Kor, Jeremy Shaw, Eric Orozco.</small>
             <br />
-            It is for educational purposes only.
+            Silver Jack
             <br />
         </footer>
     </body>
